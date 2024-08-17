7376fdf9e071a88c18fddd9fd35708bfbb75dc13
Ishara Induranga <69160726+isharainduranga@users.noreply.github.com>
2024-08-17 13:02:23 +0530

    routing commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  // configure the paths to all of your source files
+  content: [
+    'node_modules/preline/dist/*.js',
+    './src/**/*.{html,js}',
+  ],
 
+  // enable dark mode via class strategy
+  darkMode: 'class',
+
+  theme: {
+    extend: {
+      // extend base Tailwind CSS utility classes
+    },
+  },
+
+  // add plugins to your Tailwind CSS project
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('preline/plugin')
+  ],
+}
